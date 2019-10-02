30330b69b9d34031b15b605b4278e1c553197a5a
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-10-01 17:51:02 -0700

    adds yargs and some command line args

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,42 @@
-const validator = require('validator');
+const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
+const log = console.log;
 
-const msg = getNotes();
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler() {
+    log(chalk.blue('adding note'));
+  },
+});
 
-console.log(msg);
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler() {
+    log(chalk.red.bold('removing note'));
+  },
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'Listing your notes',
+  handler() {
+    log(chalk.green('All notes go here...'));
+  },
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler() {
+    log(chalk.yellow('showing a specific note now'));
+  },
+});
+
+log(yargs.argv);
